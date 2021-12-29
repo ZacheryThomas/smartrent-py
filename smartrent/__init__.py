c6f5423f8d10fa50205e955f3e9a0298509bbe76
ZacheryThomas <zacherythomas12@gmail.com>
2021-12-29 08:32:34 -0500

    __init__: Export Client
    Allows for use of client instead of having to always use `async_login` to build devices

--- smartrent/__init__.py
+++ smartrent/__init__.py
@@ -1,9 +1,11 @@
 from smartrent.api import async_login
 from smartrent.lock import DoorLock
 from smartrent.thermostat import Thermostat
+from smartrent.utils import Client
 
 __all__ = [
     "async_login",
     "DoorLock",
-    "Thermostat"
+    "Thermostat",
+    "Client"
 ]
