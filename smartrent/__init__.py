12bbcb9979872c2df962f62797f4edd4fd6da9c7
ZacheryThomas <zacherythomas12@gmail.com>
2022-02-11 19:19:56 -0500

    smartrent: Add devs to __all__

--- smartrent/__init__.py
+++ smartrent/__init__.py
@@ -1,6 +1,15 @@
 from smartrent.api import async_login
 from smartrent.lock import DoorLock
 from smartrent.thermostat import Thermostat
+from smartrent.sensor import LeakSensor
+from smartrent.switch import BinarySwitch
 from smartrent.utils import Client
 
-__all__ = ["async_login", "DoorLock", "Thermostat", "Client"]
+__all__ = [
+    "async_login",
+    "DoorLock",
+    "Thermostat",
+    "LeakSensor",
+    "BinarySwitch",
+    "Client",
+]
